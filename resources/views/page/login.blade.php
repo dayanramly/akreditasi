6f3cf1e32835548221ba87c26ff52ea75a27a1db
Dayan Ramly Ramadhan <dayanramly@gmail.com>
2015-08-13 17:08:39 +0700

    login authentification not fix

--- resources/views/page/login.blade.php
+++ resources/views/page/login.blade.php
@@ -9,10 +9,23 @@
 			<p class="flow-text">Selamat datang di sistem akreditasi.</p>
 		</div>
 	</div>
+
+	<div class="row">
+		@if(count($errors)>0)
+		<div class="alert">
+			ada masalah <br /><br />
+			<ul>
+				@foreach ($errors->all() as $error)
+				<li>{{$error}}</li>
+				@endforeach
+			</ul>
+		</div>
+		@endif
+	</div>
 	<div class="row">
 		<div class="col s6 offset-s3 center">
 			<div class="form-login z-depth-3 col s12">
-				<form autocomplete="off" method="POST" action="/login">
+				<form autocomplete="off" method="POST" action="/auth/login">
 					{!! csrf_field() !!}
 
 					<div class="input-field col s12">
