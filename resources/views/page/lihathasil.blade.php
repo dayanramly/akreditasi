d35ba0a24e9a23ae483682e5dd600753ddf075c9
Dayan Ramly Ramadhan <dayzramly@gmail.com>
2015-09-09 08:14:01 +0700

    add lihat hasil

--- resources/views/page/lihathasil.blade.php
+++ resources/views/page/lihathasil.blade.php
@@ -16,9 +16,64 @@
 		</thead>
 
 		<tbody>
-			{{-- {{{json_encode($hasil->jawaban)}}} --}}
-			{{-- @foreach (json_encode($hasil->jawaban) as $hasilList) --}}
-			{{-- {{{$hasilList}}} --}}
+			<?php $jawaban = json_decode($hasil->jawaban, true);
+			// print_r($jawaban);
+			// die();
+			?>
+			
+			@for($i=1;$i<186;$i++)
+			<?php 
+				// print_r($jawaban[$i]);
+				// die();
+			?>
+
+			@if(!empty($jawaban[$i]))
+			<tr>
+				<td>{{{$jawaban[$i]['no']}}}</td>
+				<td>{{{$jawaban[$i]['jawaban']}}}</td>
+				<td>
+					@if(!empty($jawaban[$i]['file']))
+					{{{$jawaban[$i]['file']}}}
+					@else
+					Belum diisi
+					@endif
+				</td>
+				<td>
+					@if((!empty($jawaban[$i]['jawaban']))&&(!empty($jawaban[$i]['file'])))
+						Lengkap
+					@else
+						Belum Lengkap
+					@endif
+				</td>
+				<td><a class="waves-effect waves-light btn orange" href="pertanyaan?page={{{$i}}}">Lihat</a></td>
+			</tr>
+			@else
+			<tr>
+				<td>{{{$i}}}</td>
+				<td colspan="3">
+					Belum diisi
+				</td>
+				<td><a class="waves-effect waves-light btn" href="pertanyaan?page={{{$i}}}">Isi</a></td>
+			</tr>
+			@endif
+			@endfor
+			{{-- @else --}}
+
+			{{-- @endif --}}
+
+			{{-- @endfor --}}
+{{-- 					@if()
+					@else
+
+					@endif --}}
+					
+					{{-- {{{print_r(json_decode($hasil))}}} --}}
+					{{-- {{{$hasil->jawaban}}} --}}
+{{-- 			@foreach($hasil as $hasilList)
+				{{{$hasilList}}}
+				@endforeach --}}
+				{{-- @foreach (json_encode($hasil->jawaban) as $hasilList) --}}
+				{{-- {{{$hasilList}}} --}}
 {{-- 			<tr>
 				<td>1</td>
 				<td>a</td>
